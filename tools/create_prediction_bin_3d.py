00bfac2a8627d6a028bef0d7e7d37c70748805f6
Abby Xiong <abby.xxn@outlook.com>
2021-12-29 15:23:54 +0800

    Redefine the save .pkl format

--- tools/create_prediction_bin_3d.py
+++ tools/create_prediction_bin_3d.py
@@ -1,7 +1,6 @@
 import argparse
 import pickle as pkl
 
-import numpy as np
 from waymo_open_dataset import label_pb2
 from waymo_open_dataset.protos import metrics_pb2
 
@@ -13,45 +12,51 @@ type_dict = {
     'TYPE_CYCLIST': 4,
 }
 
-cls_map = {
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 0
-}
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--work_dir', '-p', help='Path to store experiments', type=str)
+    parser.add_argument('--config_name', '-c', help='Name of the config to be tested', type=str)
+    parser.add_argument('--epoch', '-e', help='Number of epoch to be tested', type=str)
+    parser.add_argument('--save_bin_dir', '-s', help='Path to save bins', type=str)
+    args = parser.parse_args()
+    return args
 
 
-def _create_bbox_prediction(det, class_id, frame_name, marco_ts):
+def _create_bbox_prediction(bbox3d_xyzlwhyaws, pred_type_id, frame_name, marco_ts):
+    """Creates a prediction objects file."""
     o = metrics_pb2.Object()
+    # The following 3 fields are used to uniquely identify a frame a prediction
+    # is predicted at. Make sure you set them to values exactly the same as what
+    # we provided in the raw data. Otherwise your prediction is considered as a
+    # false negative.
     o.context_name = (frame_name)
+    # The frame timestamp for the prediction. See Frame::timestamp_micros in
+    # dataset.proto.
     o.frame_timestamp_micros = marco_ts
     # This is only needed for 2D detection or tracking tasks.
     # Set it to the camera name the prediction is for.
-    # o.camera_name = camera_dict[camera_name]
+    # o.camera_name = dataset_pb2.CameraName.FRONT
+
     # Populating box and score.
     box = label_pb2.Label.Box()
-    box.center_x = np.mean(det[[0, 2, 4, 6]])
-    box.center_y = np.mean(det[[1, 3, 5, 7]])
-    z0 = det[9]
-    height = det[10]
-    box.center_z = z0 + height / 2
-    box.width = np.sqrt((det[2] - det[4]) ** 2 + (det[3] - det[5]) ** 2)
-    box.length = np.sqrt((det[2] - det[0]) ** 2 + (det[3] - det[1]) ** 2)
-    box.height = height
-    box.heading = det[8]
+    box.center_x = bbox3d_xyzlwhyaws[0]
+    box.center_y = bbox3d_xyzlwhyaws[1]
+    box.center_z = bbox3d_xyzlwhyaws[2]
+    box.length = bbox3d_xyzlwhyaws[3]
+    box.width = bbox3d_xyzlwhyaws[4]
+    box.height = bbox3d_xyzlwhyaws[5]
+    box.heading = bbox3d_xyzlwhyaws[6]
     o.object.box.CopyFrom(box)
-
     # This must be within [0.0, 1.0]. It is better to filter those boxes with
     # small scores to speed up metrics computation.
-    if len(det) == 12:
-        o.score = det[11]  # 1/(1 + np.exp(-1 * det[11]))
+    if len(bbox3d_xyzlwhyaws) == 8:
+        o.score = bbox3d_xyzlwhyaws[7]
     # For tracking, this must be set and it must be unique for each tracked
     # sequence.
     o.object.id = ''
     # Use correct type.
-
-    o.object.type = cls_map[class_id]
+    o.object.type = pred_type_id
     return o
 
 
@@ -70,43 +75,34 @@ def _create_pd_file_example(obj_list, filename):
     f.close()
 
 
-def main(exp_path, name, bins_path):
-    with open(exp_path, "rb") as fr:
+def main(pred_boxes_path, config_name, save_bin_dir):
+    print(f'Load the prediction boxes from this address: {pred_boxes_path}')
+    with open(pred_boxes_path, "rb") as fr:
         annotation_dict = pkl.load(fr)
         output_dict = pkl.load(fr)
-    np.set_printoptions(
-        suppress=True,
-        formatter={'float_kind': '{:0.2f}'.format})
 
-    pred_list = []
-    count = 0
-    for rec_id, output in output_dict.items():
-        count += 1
-        if count % 5000 == 0:
-            print(count, '/', len(output_dict))
+    pred_bbox3d_list = []
+    for count, (rec_id, output) in enumerate(output_dict.items()):
+        if count % 1000 == 0:
+            print(f'{count} frames have been processed, a total of {len(output_dict)} frames')
+        if len(output) == 0:
+            continue
 
-        frame_name = output['pc_url'].split('/')[-2].replace('segment-', '').replace('_with_camera_labels', '')
-        ts = int(output['pc_url'].split('/')[-1][:-4])
+        for pred_type, pred_bboxes3d in output['det_xyzlwhyaws'].items():
+            for pred_bbox3d in pred_bboxes3d:
+                pred_bbox3d_list.append(
+                    _create_bbox_prediction(
+                        pred_bbox3d,
+                        type_dict[pred_type],
+                        output['meta_info']['name'],
+                        output['meta_info']['timestamp_micros']))
 
-        for i in range(1, 6):
-            if i in output['det_4pts'].keys():
-                for bbox in output['det_4pts'][i]:
-                    pred_list.append(_create_bbox_prediction(bbox, i, frame_name, ts))
-    _create_pd_file_example(pred_list, '{}/{}.bin'.format(bins_path, name))
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--exp_path', '-p', help='path to experiments', type=str, default='')
-    parser.add_argument('--config', '-c', help='config name', type=str)
-    parser.add_argument('--epoch', '-e', help='the epoch number to test', type=str)
-    parser.add_argument('--save_bin_path', '-e', help='path to save bins', type=str)
-    args = parser.parse_args()
-    return args
+    _create_pd_file_example(
+        pred_bbox3d_list, '{}/{}.bin'.format(save_bin_dir, config_name))
 
 
 if __name__ == '__main__':
     args = parse_args()
-    exp_path = '{}/{}/checkpoint_output_dict_{}e.pkl'.format(
-        args.exp_path, args.config, args.epoch)
-    main(exp_path, args.config, args.save_bin_path)
+    pred_boxes_path = '{}/{}/checkpoint_output_dict_{}e.pkl'.format(
+        args.work_dir, args.config_name, args.epoch)
+    main(pred_boxes_path, args.config_name, args.save_bin_dir)
